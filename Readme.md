93c2a1acdf3155cc5ead687fe697b4ffbaf8ea30
Donald Cook <donald@stepchangegroup.com>
2011-12-31 17:28:33 -0800

    init commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# strap.js
+
+strap.js is a super basic bootstrap for Node.js.
+
+## Getting started
+
+All that's required is Node.js and npm
+
+Run `npm install`
+
+Then run `node app.js`
+
+Direct your browser to http://localhost:3000
+
+And you should be up!
---
+++ app.js
@@ -0,0 +1,30 @@
+require.paths.unshift(__dirname + '/lib');
+
+var express   = require('express');
+
+// create an express server
+var app = express.createServer(
+  express.logger(),
+  express.static(__dirname + '/public'),
+  express.cookieParser(),
+  // set this to a secret value to encrypt session cookies
+  express.session({ secret: process.env.SESSION_SECRET || 's1llyc4t' })
+);
+
+// listen to the PORT given to us in the environment
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+// respond to GET /
+app.get('/', function(request, response) {
+
+  // render the home page
+  response.render('index.ejs', {
+    layout:   true,
+    app:      app
+  });
+
+});
